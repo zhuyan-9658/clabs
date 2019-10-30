7be1a2bdf76739ad6441aac905720d052623c22e
zhuyan-9658 <1398131736@qq.com>
2019-10-30 06:39:23 -0700

    added a ctof.c file

--- labi/ctof.c
+++ labi/ctof.c
@@ -1,18 +1,19 @@
-#include  <stdio.h>
+#include <stdio.h>
 
 main()
 {
-    int  celsius,  fahr;
-    int  lower, upper, step;
+    int celsius, fahr;
+    int lower, upper, step;
 
-    lower = 0:
+    lower = 0;
     upper = 100;
     step = 20;
 
     celsius = lower;
     while (celsius <=upper) {
         fahr = celsius*9/5+32;
-        printf ("%d\t%d\n", fahr,celsius);
+        printf ("%d\t%d\n",fahr,celsius);
         celsius=celsius+step;
     }
 }        
+    
