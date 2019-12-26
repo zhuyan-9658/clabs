ecba526d0b6ca7e198eda7e5210439356d402441
zhuyan-9658 <1398131736@qq.com>
2019-12-25 21:17:48 -0800

    added a bc.c file

--- labi/lab2/bc.c
+++ labi/lab2/bc.c
@@ -1,31 +1,20 @@
 #include <stdio.h>
-#inchude <stdlib.h>
 
-int maim(void)
+int main()
 {
-     int n;
-     int m=0:
-     int c=[100];
      int count=0;
-     printf("enter a four-digit positive int:\n");
-     scanf("%d",&n);
-     printf("binary system");
-     while(n)
+     int m=0;
+     int a=0;
+     printf("please input the m\n");
+     scanf("%d",&a);
+     while(a !=0)
      {
-          c[m]=n%2;
-          m++;
-          n/=2;
-          
-        
-     }   
-     for(int j=m-1;j>=0;j--)   
-     {
-         printf("%d",c[j]);
-         if(c[j]==1){
-            count++;
-         }
+          m=a%2;
+          a=a/2;
+          if(m ==1)
+          count++;
+         
      }
-     printf("\n\"1\"the numder of occurrences =%d\n",count);
-     system("pause");
+     printf("the amount is:%d\n",count);
      return 0;
 }     
